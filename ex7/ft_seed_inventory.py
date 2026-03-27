d0bb02706e14034e76737461aa421458c02a012d
nanjarinony-byte <nanjarinony@gmail.com>
2026-03-27 13:47:01 +0300

    Add ex7

---
+++ ex7/ft_seed_inventory.py
@@ -0,0 +1,11 @@
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    seeds = f"{seed_type} seeds: "
+    if unit == "packets":
+        seeds += f"{quantity} packets available"
+    elif unit == "grams":
+        seeds += f"{quantity} grams total"
+    elif unit == "area":
+        seeds += f"covers {quantity} square meters"
+    else:
+        seeds = "Unknown unit type"
+    print(f"{seeds.capitalize()}")
